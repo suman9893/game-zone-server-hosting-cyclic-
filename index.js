17d7f7fdc2ec62a963afd696d49262e00a8e92be
Suman Mondal <70449874suman@gmail.com>
2024-06-23 13:07:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(morgan("dev"));
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT,
+};
+app.use(cors(corsOptions));
 app.use("/users", userRouter);
 app.use("/tour", tourRouter);
 app.get("/", (req, res) => {
